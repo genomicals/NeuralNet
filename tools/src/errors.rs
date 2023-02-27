def071e4ba85c47df1865395fedc42026db805d9
nbalcarc <nathan.balcarcel@wsu.edu>
2023-02-27 02:15:38 -0800

    NeuralNetwork weights are randomly generated and stored

--- tools/src/errors.rs
+++ tools/src/errors.rs
@@ -3,12 +3,16 @@ use core::fmt;
 /// Custom error type for the NeuralNet library
 #[derive(Debug)]
 pub enum NeuralNetError {
-
+    InvalidInputSize,
+    InvalidWeightSize,
 }
 impl std::error::Error for NeuralNetError {}
 impl fmt::Display for NeuralNetError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Errors to be implemented")
+        match self {
+            Self::InvalidInputSize => write!(f, "Input was not size 32"),
+            Self::InvalidWeightSize => write!(f, "Input was not size _"),
+        }
     }
 }
 
--- tools/src/neural_network.rs
+++ tools/src/neural_network.rs
@@ -1,17 +1,58 @@
 use pyo3::prelude::*;
+use rand::Rng;
+
+use crate::errors::NeuralNetError;
+
+
+const INPUT_SIZE: usize = 32;
+const WEIGHT_SIZE: usize = 17322; //layers: 32, 64, 64, 170;   33*64 + 65*64 + 65*170 = 17322
+
 
 /// Neural network struct
 pub struct NeuralNetwork {
-    //weights: i32,
+    /// Weights and biases for every perceptron
+    weights: [f32; WEIGHT_SIZE],
 }
 impl NeuralNetwork {
+
+    /// Create a new NeuralNetwork with randomized weights
     pub fn new() -> Self {
-        NeuralNetwork {}
+        let weights: [f32; WEIGHT_SIZE] = [(); WEIGHT_SIZE].map(|_| rand::thread_rng().gen_range(-2.0..=2.0));
+        NeuralNetwork {weights}
     }
 
-    /// Run the input through the neural network
-    pub fn calculate(&self, input: &[i8]) -> Vec<f32> {
-        Vec::with_capacity(170)
+
+    /// Create a new NeuralNetwork with the provided weights
+    pub fn with_weights(weights: [f32; WEIGHT_SIZE]) -> Self {
+        NeuralNetwork {weights}
+    }
+
+
+    /// Run the input through the neural network, consider stripping for runtime reasons
+    pub fn calculate(&self, input: &[i8]) -> Result<[f32; 170], NeuralNetError> {
+        if input.len() != INPUT_SIZE {
+            return Err(NeuralNetError::InvalidInputSize);
+        } else if self.weights.len() != WEIGHT_SIZE {
+            return Err(NeuralNetError::InvalidWeightSize);
+        }
+        let mut output: [f32; 170] = [0.0; 170];
+
+        
+
+
+
+
+        Ok(output)
+    }
+
+    /// Set the weights
+    pub fn set_weights(&mut self, weights: [f32; WEIGHT_SIZE]) -> Result<(), NeuralNetError> {
+        if weights.len() != WEIGHT_SIZE {
+            Err(NeuralNetError::InvalidWeightSize)
+        } else {
+            self.weights = weights;
+            Ok(())
+        }
     }
 }
 
