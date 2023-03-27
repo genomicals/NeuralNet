75d8085d4793036bcb8f92540c0eb64cd5bf6f0e
BlakeCoulter <BlakeCoulter8@hotmail.com>
2023-03-26 20:45:44 -0700

    Added reproduce function to AI.
    Co-authored-by: Nathan Balcarcel <nbalcarc@users.noreply.github.com>

--- tools/src/neural_network.rs
+++ tools/src/neural_network.rs
@@ -1,88 +1,74 @@
 use rand::Rng;
-use libm;
 
-use crate::errors::NeuralNetError;
 
 /// Neural network struct
 pub struct NeuralNetwork {
-    /// The shape of the neural network
-    shape: Vec<usize>,
     /// Weights and biases for every perceptron
-    weights: Vec<f32>,
-    /// Largest layer size
-    max_size: usize,
+    weights: [f32; 17322],
 }
 impl NeuralNetwork {
 
     /// Create a new NeuralNetwork with randomized weights
     pub fn new() -> Self {
-        let shape = vec![32, 64, 64, 170];
-        let weights: Vec<f32> = [(); 17322].map(|_| rand::thread_rng().gen_range(-2.0..=2.0)).to_vec();
-        NeuralNetwork {shape, weights, max_size: 170}
+        let weights: [f32; 17322] = [(); 17322].map(|_| rand::thread_rng().gen_range(-2.0..=2.0));
+        NeuralNetwork {weights}
     }
 
 
     /// Create a new NeuralNetwork with the provided weights
-    pub fn with_weights(shape: Vec<usize>, weights: Vec<f32>) -> Self {
-        let max_size = shape.iter().max().unwrap_or_else(|| &0).to_owned();
-        NeuralNetwork {shape, weights, max_size}
+    pub fn with_weights(weights: [f32; 17322]) -> Self {
+        NeuralNetwork {weights}
     }
 
 
     /// Create a new NeuralNetwork from the provided genome
     pub fn from_genome(genome: &[f32; 17323]) -> Self {
-        let weights: Vec<f32> = Vec::from(&genome[..17322]);
-        let shape = vec![32, 64, 64, 170];
-        NeuralNetwork {shape, weights, max_size: 170}
-
+        let mut weights: [f32; 17322] = [0.0; 17322];
+        weights.copy_from_slice(&genome[..17322]); //copy weights from genome into our neural network
+        NeuralNetwork::with_weights(weights)
     }
 
 
     /// Run the input through the neural network, consider stripping for runtime reasons
-    pub fn calculate(&self, input: &Vec<f32>) -> Result<Vec<f32>, NeuralNetError> {
-        if input.len() != self.shape[0] {
-            return Err(NeuralNetError::InvalidInputSize);
-        }
+    pub fn calculate(&self, input: &[f32; 32]) -> [f32; 170] {
+        let mut output: [f32; 170] = [0.0; 170]; //used for first layer and output
+        let mut internal: [f32; 64] = [0.0; 64]; //used for second layer
 
-        let mut mat0: Vec<f32> = vec![0.0; self.max_size];
-        let mut mat1: Vec<f32> = input.clone(); //assumes the input is the right size
-        mat1.extend(vec![0.0; self.max_size-self.shape[0]].iter()); //make the input the right size
-
-        // calculate forward propagation
-        let mut layer_offset = 0; //offset for each layer's weights from the start of the vector
-        let mut weight_size; //size of one perceptron's inputs with bias
-        let mut weight_offset; //location of current weights
-
-        for n in 1..self.shape.len() { //for each layer
-            std::mem::swap(&mut mat0, &mut mat1); //swap the two vectors
-            weight_size = self.shape[n-1]+1;
-            for i in 0..self.shape[n] { //for each perceptron
-                mat1[i] = 0.0;
-                weight_offset = layer_offset + i*weight_size;
-                for j in 0..weight_size-1 { //for each input
-                    mat1[i] += mat0[j] * self.weights[weight_offset + j]; //apply weight to input
-                }
-
-                mat1[i] += self.weights[weight_offset + weight_size - 1]; //apply bias
-                mat1[i] = libm::tanh(mat1[i] as f64) as f32; //apply tanh normalization                                                                          
+        // first layer
+        for i in 0..64 { //for each perceptron
+            for j in 0..32 { //for each input
+                output[i] += input[j] * self.weights[i*33 + j]; //apply weight to input
             }
-            layer_offset += self.shape[n-1] * (self.shape[n]+1);
+            output[i] += self.weights[i*33 + 32]; //apply bias
+            output[i] = output[i].tanh(); //apply tanh normalization
+        } 
+
+        // second layer
+        for i in 0..64 { //for each perceptron
+            for j in 0..64 { //for each input
+                internal[i] += output[j] * self.weights[2112 + i*65 + j] //apply weight to input
+            }
+            internal[i] += self.weights[2112 + i*65 + 64]; //apply bias
+            internal[i] = internal[i].tanh(); //apply tanh normalization
         }
 
-        Ok(mat1)
+        //output layer
+        for i in 0..170 { //for each perceptron
+            output[i] = 0.0; //reset the element after being used for layer 1
+            for j in 0..64 { //for each input
+                output[i] += internal[j] * self.weights[6272 + i*65 + j] //apply weight to input
+            }
+            output[i] += self.weights[6272 + i*65 + 64]; //apply bias
+            output[i] = output[i].tanh() * 0.5 + 1.0; //apply sigmoid normalization, consider tanh for runtime reasons
+        }
+
+        output
     }
 
 
     /// Set the weights
-    pub fn set_weights(&mut self, weights: Vec<f32>) -> Result<(), NeuralNetError> {
-        //if self.shape[1..].iter().map(|x| x+1).product::<usize>() * self.shape[0] != weights.len() {
-        //    return Err(NeuralNetError::InvalidWeightSize);
-        //}
-        //if self.shape[1..].iter().fold(self.shape[0], |accum, x| (accum+1) * x) != weights.len() {
-        //    return Err(NeuralNetError::InvalidWeightSize);
-        //} 
+    pub fn set_weights(&mut self, weights: [f32; 17322]) {
         self.weights = weights;
-        Ok(())
     }
 }
 
